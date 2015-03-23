d6853d257be3f64a539e2da5cc637bd1657c468a
Mickael Vieira <contact@mickael-vieira.com>
2015-03-23 20:08:40 +0000

    Added License

--- src/Imagery/Extractor/Exif.php
+++ src/Imagery/Extractor/Exif.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace Imagery\Extractor;
+/*
+ * This file is part of CollectionJson, a php implementation
+ * of the Collection+JSON Media Type
+ *
+ * (c) Mickaël Vieira <contact@mickael-vieira.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+namespace Imagery\Extractor;
 
 use Imagery\Extractor;
 
+/**
+ * Class Exif
+ * @package Imagery\Extractor
+ */
 final class Exif implements Extractor
 {
     /**
