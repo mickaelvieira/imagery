2bb1b5f346b31a1fce2a58ab88893a0d6e4530f6
Mickael Vieira <contact@mickael-vieira.com>
2015-03-24 14:40:45 +0000

    Added missing license and PHPDocs

--- src/Imagery/Command/Resize.php
+++ src/Imagery/Command/Resize.php
@@ -1,12 +1,24 @@
 <?php
 
+/*
+ * This file is part of CollectionJson, a php implementation
+ * of the Collection+JSON Media Type
+ *
+ * (c) Mickaël Vieira <contact@mickael-vieira.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Imagery\Command;
-
 
 use Imagery\Command;
 use Imagery\Options;
 
+/**
+ * Class Resize
+ * @package Imagery\Command
+ */
 final class Resize implements Command
 {
     /**
