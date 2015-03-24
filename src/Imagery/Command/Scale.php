c3ecdf01f07d71580ee9cecd2ffb3c13356a646d
Mickael Vieira <contact@mickael-vieira.com>
2015-03-24 22:45:42 +0000

    Added license and PHPDocs

--- src/Imagery/Command/Scale.php
+++ src/Imagery/Command/Scale.php
@@ -1,12 +1,24 @@
 <?php
 
+/*
+ * This file is part of CollectionJson, a php implementation
+ * of the Collection+JSON Media Type
+ *
+ * (c) Mickaël Vieira <contact@mickael-vieira.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Imagery\Command;
-
 
 use Imagery\Command;
 use Imagery\Options;
 
+/**
+ * Class Scale
+ * @package Imagery\Command
+ */
 final class Scale implements Command
 {
     /**
@@ -22,7 +34,11 @@ final class Scale implements Command
      */
     public function execute($resource, Options $options = null)
     {
-        $scale = $options->get('scale', 100);
+        $scale = $options->get('scale');
+
+        if (is_null($scale)) {
+            return $resource;
+        }
 
         $srcWidth  = imagesx($resource);
         $srcHeight = imagesy($resource);
